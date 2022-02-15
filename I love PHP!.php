ebd198d5e73a7cbd89c8f6409ade141ac0d25d85
Alexbotduglas <52333919+Alexbotduglas@users.noreply.github.com>
2022-02-15 15:00:26 +0300

    Add files via upload

---
+++ I love PHP!.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>My first PHP page</title>
+  </head>
+  <body>
+  <?php   
+    echo "I love PHP!";
+  ?>
+  </body>
+</html>
---
+++ STEPIK.php
@@ -0,0 +1,3 @@
+<?php
+  echo "<h1>Stepik</h1>";
+?>
